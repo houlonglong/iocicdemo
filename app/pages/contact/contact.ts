e9a3a7bf573fd0c99542be2290245b2e4607e69d
longlong <31363537@qq.com>
2016-08-19 07:03:20 +0800

    'lianxi'

--- app/pages/contact/contact.ts
+++ app/pages/contact/contact.ts
@@ -1,13 +1,13 @@
-import {Component} from '@angular/core';
-import {NavController,Loading} from 'ionic-angular';
-
+import {Component,ViewEncapsulation} from '@angular/core';
+import {ionicBootstrap,NavController,Loading,Alert,Toast,Modal} from 'ionic-angular';
+import {HomePage} from '../home/home';
 @Component({
   templateUrl: 'build/pages/contact/contact.html'
 })
 export class ContactPage {	
 	
     public user = {
-    	username : '',
+    	username :'',
     	password:''
     }
    
@@ -16,12 +16,35 @@ export class ContactPage {
   		this.navCtrl = navCtrl;
   }
   login(){
-  		console.log(this.user)
-  		let loading = Loading.create({
-  			content:'加载中',
-  			duration:3000,
-  			dismissOnPageChang:true
+  	if(this.user.username.length == 0){
+  		console.log(Toast,this.user)
+  		let ToastUserNameError = Toast.create({
+  			 message: '您输入用户名不正确',
+     		 duration: 3000,
+     		 position: 'middle',
+
   		})
-  		this.navCtrl.present(loading)
+  		// let AlertUserNameError =Alert.create({
+  		// 	title: '龙龙干姬晴',
+		  //   subTitle: '龙龙干姬晴龙龙干姬晴龙龙干姬晴',
+		  //   buttons:['爽不爽']
+		    
+  		// })
+  		 this.navCtrl.present(ToastUserNameError)
+  		  		return;
+  	}
+  	let loading = Loading.create({
+      spinner: 'dots',
+      content: '正在登陆中...',
+      duration: 3000
+    });
+
+    this.navCtrl.present(loading);
+  }
+  register(){
+  	console.log(Modal)
+
+      var modal = Modal.create(HomePage);	
+      this.navCtrl.present(modal);
   }
 }
